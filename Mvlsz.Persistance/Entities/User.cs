ee764d1d2263c6e36aa32d656139d43692726705
Zsolt Hangay <hangay.zsolt@outlook.com>
2020-06-13 18:56:04 +0200

    Persistance project upgrade to core 3.1

--- Mvlsz.Persistance/Entities/User.cs
+++ Mvlsz.Persistance/Entities/User.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 using Microsoft.AspNetCore.Identity;
 
 namespace Mvlsz.Persistance.Entities
